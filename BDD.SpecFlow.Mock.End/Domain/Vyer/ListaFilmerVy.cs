3a76347c4190c21897de64cce9235d78af4a3655
Marcus Hammarberg <marcusoft.net@gmail.com>
2010-02-26 20:59:34 +0100

    Implemented feature 2 - but with failing verification, moq is mocking me ;)

---
+++ BDD.SpecFlow.Mock.End/Domain/Vyer/ListaFilmerVy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using BDD.SpecFlow.Mock.End.Domain.Model.Entitet;
+
+namespace BDD.SpecFlow.Mock.End.Domain.Vyer
+{
+    public class ListaFilmerVy : Vy
+    {
+        private readonly TextWriter _textWriter;
+
+        public ListaFilmerVy(TextWriter textWriter)
+        {
+            _textWriter = textWriter;
+        }
+
+        public string Meddelande
+        {
+            get { return string.Empty; }
+        }
+
+        public void Presentera(IList<Film> filmLista)
+        {
+            foreach (var film in filmLista)
+            {
+                _textWriter.WriteLine(film.Namn);
+            }
+
+            _textWriter.WriteLine("Du har {0} filmer i samlingen", filmLista.Count);
+        }
+
+    }
+}
---
+++ BDD.SpecFlow.Mock.End/Egenskaper/Filmsamling/Lista_filmer.feature
@@ -0,0 +1,15 @@
+﻿#language: se
+Egenskap: Lista filmer
+	För att få en överblick av min samling
+	som filmsamlare 
+	vill jag kunna lista alla filmer i mitt bibliotek samt se totala antal filmer 
+
+Scenario: "Alien", "E.T." och "Jaws" i filmsamlingen
+	Givet att filmsamlingen innehåller "Alien", "E.T.", "Jaws"
+	När jag anger kommando: ListaFilmer
+	Så ska resultatet vara:
+	| Rad |
+	| Alien |
+	| E.T. |
+	| Jaws |
+	| Du har 3 filmer i samlingen |
